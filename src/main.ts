4623609a34fc16d2aeb1dc443f0300e2597e98e6
jawwadkareem <karimjawwad09@gmail.com>
2025-12-26 12:33:41 +0500

    ..

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(7008);
+  await app.listen(7009);
 }
 bootstrap();
